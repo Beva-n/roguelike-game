0286b452d0a95d71c267b153118006a8cec4fb17
brian <brianwu312@gmail.com>
2023-07-17 02:47:59 -0700

    Minor code reworks and added tests

---
+++ src/main/model/powerups/AttackBlessing.java
@@ -0,0 +1,26 @@
+package model.powerups;
+
+import model.Game;
+import model.PowerUp;
+
+public class AttackBlessing extends PowerUp {
+
+    public static final String NAME = "ATK BOOST";
+    public static final String DESCRIPTION = "Increase attack by 5";
+
+    //Effects: Construct a one time use attack up powerup with a name, description
+    public AttackBlessing(Game g) {
+        super(true, NAME, DESCRIPTION, g);
+    }
+
+    //Modifies: game
+    //Effects: Increases the player's attack by 5
+    @Override
+    public void apply() {
+        game.getPlayer().editAttack(5);
+    }
+
+//    @Override
+//    public void drawCard(Screen screen) {
+//    }
+}
