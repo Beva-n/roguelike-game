65cf5f95ff21248e04cb48c90b620421da51866c
brian <brianwu312@gmail.com>
2023-08-07 16:56:35 -0700

    Added tests

--- src/main/model/projectiles/BlueRotationalProjectile.java
+++ src/main/model/projectiles/BlueRotationalProjectile.java
@@ -7,7 +7,8 @@ import ui.Vector;
 import java.awt.*;
 
 public class BlueRotationalProjectile extends Projectile {
-    public BlueRotationalProjectile(Position position, double dx, double dy, int damage, int lifetime, int limit, Game game) {
+    public BlueRotationalProjectile(Position position,
+                                    double dx, double dy, int damage, int lifetime, int limit, Game game) {
         super(position, damage, lifetime, game);
         setVector(new Vector(dx, dy, 25, limit));
         width = 18;
